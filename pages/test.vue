6d26dfe5d7b2f37c2b26b108214713fce3d604c1
Kris <c-text@yandex.ru>
2021-01-29 12:34:54 +0300

    first commit

---
+++ pages/test.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <form @submit.prevent="userLogin">
+      <div>
+        <label>Username</label>
+        
+        <v-text-field
+            v-model="login.username"
+            label="Outlined"
+            placeholder="Placeholder"
+            outlined
+          />
+      </div>
+      <div>
+        <label>Password</label>
+        
+        <v-text-field
+            v-model="login.password"
+            label="Outlined"
+            placeholder="Placeholder"
+            outlined
+          />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async userLogin() {
+      try {
+        let response = await this.$auth.loginWith('local', { data: this.login })
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
